ac397b943721281277e8c8c640003c92c852a230
Radhityawahyu <radhityawahyu42@gmail.com>
2025-06-20 20:02:20 +0700

    Tugas10

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,35 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/produk', 'ProdukController::index');
-$routes->get('/keranjang', 'TransaksiController::index');
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProductController::index');
+    $routes->post('', 'ProductController::create');
+    $routes->post('edit/(:any)', 'ProductController::edit/$1');
+    $routes->get('delete/(:any)', 'ProductController::delete/$1');
+    $routes->get('download', 'ProductController::download');
+});
+
+// Route kategori produk diluar group 'produk', jadi URL-nya langsung /ProductCategory
+$routes->get('ProductCategory', 'ProductCategoryController::index');
+$routes->post('ProductCategory', 'ProductCategoryController::create');
+$routes->post('ProductCategory/edit/(:any)', 'ProductCategoryController::edit/$1');
+$routes->get('ProductCategory/delete/(:any)', 'ProductCategoryController::delete/$1');
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+});
+
